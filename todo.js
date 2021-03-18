5d3e1db17ad1974963151e8818e92fb2c15376dc
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2021-03-18 15:21:29 +0200

    new version

--- todo.js
+++ todo.js
@@ -1,178 +1,163 @@
-
-// DATA & TIME
-function getCurentDate() {
-  let date = new Date();
-  let year= date.getFullYear();
-  let days = date.getDate();
-  // if (month==1) {
-  //     month="Януари"
-  // } else if (month==2) {
-  //     month="Февруари"
-  // } else if (month==3) {
-  //     month="Март"
-  // } else if (month==4) {
-  //     month="Април"
-  // } else if (month==5) {
-  //     month="Май"
-  // } else if (month==6) {
-  //     month="Юни"
-  // } else if (month==7) {
-  //     month="Юли"
-  // } else if (month==8) {
-  //     month="Август"
-  // } else if (month==9) {
-  //     month="Септември"
-  // } else if (month==10) {
-  //     month="Октомври"
-  // } else if (month==11) {
-  //     month="Ноември"
-  // } else if (month==12) {
-  //     month="Декември"
-  // } 
-  let months = new Array(12);
-  months[0] = "Яну";
-  months[1] = "Фев";
-  months[2] = "Март";
-  months[3] = "Апр";
-  months[4] = "Май";
-  months[5] = "Юни";
-  months[6] = "Юли";
-  months[7] = "Авг";
-  months[8] = "Сеп";
-  months[9] = "Окт";
-  months[10] = "Ное";
-  months[11] = "Дек";
-  let month = months[date.getMonth()];
-  
-  let weekdays = new Array(7);
-  weekdays[0] = "Нд";
-  weekdays[1] = "Пн";
-  weekdays[2] = "Вт";
-  weekdays[3] = "Ср";
-  weekdays[4] = "Чт";
-  weekdays[5] = "Пт";
-  weekdays[6] = "Сб";
-  let weekday = weekdays[date.getDay()];
-  
-  return (`${weekday} ${days} ${month} ${year}`)
-}
-function getCurrentTime() {
-  let today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
-  let x=0;
-  if (hours<10) {
-    hours = `${x}${hours}`
-  }
-  if (minutes<10) {
-    minutes = `${x}${minutes}`
-  }
-  if (seconds<10) {
-    seconds = `${x}${seconds}`
-  }
-  let time = document.querySelector('.time');
-  time.innerHTML = `${hours}:${minutes}:${seconds}`;
-  return `${hours}:${minutes}:${seconds}`
+let dom = {
+  btnOK: document.querySelector("#btnOK"),
+  inputText: document.querySelector(".inputText"),
+  date: document.querySelector('.date'),
+  time: document.querySelector('.time'),
+  activeTasks: document.querySelector(".active"),
+  totalTasks: document.querySelector(".total"),
+  tasksContainer: document.querySelector("#tasksContainer"),
 };
-let time = setInterval(getCurrentTime, 1000);
-
-let date = document.querySelector('.date');
-date.innerHTML = getCurentDate();
-
-//  TRAVERSE DOM & COUNT FINISHED TASKS
-let finished;
-
-function countFinishedTasks() {
-  finished = 0;
-  let startNode = document.querySelector("#tasksContainer");
-  
-  function doSomething(node) {
-    if (node.classList.contains("green")) {
-      finished++;
+let globalVars = {
+  count: 0,
+  finished: undefined,
+};
+let dateAndTime = {
+  getCurentDate() {
+    let date = new Date();
+    let year= date.getFullYear();
+    let days = date.getDate();
+    let months = new Array(12);
+    months[0] = "Яну";
+    months[1] = "Фев";
+    months[2] = "Март";
+    months[3] = "Апр";
+    months[4] = "Май";
+    months[5] = "Юни";
+    months[6] = "Юли";
+    months[7] = "Авг";
+    months[8] = "Сеп";
+    months[9] = "Окт";
+    months[10] = "Ное";
+    months[11] = "Дек";
+    let month = months[date.getMonth()];
+    
+    let weekdays = new Array(7);
+    weekdays[0] = "Нд";
+    weekdays[1] = "Пн";
+    weekdays[2] = "Вт";
+    weekdays[3] = "Ср";
+    weekdays[4] = "Чт";
+    weekdays[5] = "Пт";
+    weekdays[6] = "Сб";
+    let weekday = weekdays[date.getDay()];
+    return (`${weekday} ${days} ${month} ${year}`)
+  },
+  getCurrentTime() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    if (hours<10) {
+      hours = `0${hours}`
     }
-  };
-  function traverseDOM(node) {
-    let children = node.children;
-    for (let i = 0; i < children.length; i++) {
-      let el = children[i];
-      doSomething(el);
-      traverseDOM(el);
+    if (minutes<10) {
+      minutes = `0${minutes}`
     }
-  } 
-  traverseDOM(startNode);
-}
-
-// NEW TASKS
-
-let btnOK = document.querySelector("#btnOK");
-let inputText = document.querySelector(".inputText");
-let count = 0;
-
-function createTasks() {
-  count++;
-  
+    if (seconds<10) {
+      seconds = `0${seconds}`
+    }
+    dom.time.innerHTML = `${hours}:${minutes}:${seconds}`;
+    // return `${hours}:${minutes}:${seconds}`
+  }
+};
+let display = {
+  totalTasks() {
+    dom.totalTasks.innerHTML = `ОБЩО: ${globalVars.count} задачи`;
+  },
+  finishedTasks() {
+    dom.activeTasks.innerHTML = `ЗАВЪРШЕНИ: ${globalVars.finished} задачи`;
+  },
+  countFinishedTasks() {
+    globalVars.finished = 0;
+    
+    function findFinished(node) {
+      if (node.classList.contains("green")) {
+        globalVars.finished++;
+      }
+    };
+    function traverseDOM(node) {
+      let children = node.children;
+      for (let i = 0; i < children.length; i++) {
+        let el = children[i];
+        findFinished(el);
+        traverseDOM(el);
+      }
+    } 
+    traverseDOM(dom.tasksContainer);
+  }
+};
+function TasksFactory (identificator) {
   let checkBox = document.createElement("I");
   checkBox.className = "fa fa-check-circle";
   let trash = document.createElement("I");
   trash.className = "fa fa-trash-alt";
-  let newTaskText = inputText.value;
-  let textnode = document.createTextNode(newTaskText);  
+  // let newTaskText = dom.inputText.value;
+  let textNode = document.createTextNode(dom.inputText.value);  
   let li = document.createElement("LI");
-  li.id = `id${count}`;
+  li.id = identificator;
   li.appendChild(checkBox);   
-  li.appendChild(textnode);
+  li.appendChild(textNode);
   li.appendChild(trash);   
-  document.querySelector("#tasksContainer").appendChild(li)
-  inputText.value="";
-  
-  let btnTrash = document.querySelector(`#id${count}>.fa-trash-alt`);
-  let btnCheckBox = document.querySelector(`#id${count}>.fa-check-circle`);
-  let totalTasks = document.querySelector(".total");
-  totalTasks.innerHTML = `ОБЩО: ${count} задачи`;
-  
-  // DISPLAY FINISHED
-
-  function displayFinished() {
-    let activeTasks = document.querySelector(".active");
-    activeTasks.innerHTML = `ЗАВЪРШЕНИ: ${finished} задачи`;
-  }
-  // DELETE TASKS
-
-  function removeTasks(){
-    let deleteObj = document.getElementById(`${li.id}`);
-    deleteObj.remove(); 
-    count--;
-    totalTasks.innerHTML = `ОБЩО: ${count} задачи`;
-    countFinishedTasks();
-    displayFinished();
-  }
-  
-  // MARK FINISHED TASKS
-  
-  function makeItGreen(){
-    // let makeGreen = document.querySelector(`#${li.id}>.fa-check-circle`);
-    let liStroke = document.querySelector(`#${li.id}`);
-    if (liStroke.className === "") {
-      liStroke.className = "green";
-      countFinishedTasks();
-      displayFinished();
-    } else if (liStroke.className === "green") {
-      liStroke.className = "";
-      countFinishedTasks()
-      displayFinished(); 
-    }
-  }
-  btnTrash.addEventListener('click', removeTasks)
-  btnCheckBox.addEventListener(`click`, makeItGreen)
+  dom.tasksContainer.appendChild(li)
 };
+let tasks = {
+  generateRandomNumber() {
+    return ( Math.round(Math.random() * ( 999 - 100 ) + 100 ) )
+  },
+  create() {
+    if (dom.inputText.value === "") {
+      alert("Не може да създавате празни задачи.");
+      dom.inputText.focus();
+    } else if (globalVars.count > 14) {
+      alert("Най-много по 15 задачи на ден.");
+    } else { 
+      globalVars.count++;
+      let randomNumber = tasks.generateRandomNumber();
+      let identificator = 'id'+randomNumber;
+      new TasksFactory(identificator);
+      dom.inputText.value="";
+      display.totalTasks();
+    }
+  },
+  markFinished(id){
+    let select = document.querySelector(`#${id}`);
+    if (select.className === ""){
+      select.classList.add('green');
+    } else if (select.className === 'green'){
+      select.classList.remove("green");
+    }
+    display.countFinishedTasks();
+    display.finishedTasks();
+  },
+  delete(id){
+    document.getElementById(`${id}`).remove(); 
+    globalVars.count--;
+    display.countFinishedTasks();
+    display.finishedTasks();
+    display.totalTasks();
+  }
+};
+setInterval(dateAndTime.getCurrentTime, 1000);
+dom.date.innerHTML = dateAndTime.getCurentDate();
 
-btnOK.addEventListener('click', createTasks);
-
-inputText.addEventListener("keypress", function(e) {
+dom.inputText.focus();
+dom.btnOK.addEventListener('click', tasks.create);
+dom.inputText.addEventListener("keypress", function(e) {
   if (e.key === 'Enter') {
-    createTasks()
-    inputText.value = "";
+    tasks.create()
+    dom.inputText.value = "";
   }
 });
-
+document.addEventListener("click", function(e) {
+  let id;
+  if (e.target.tagName === "LI") {
+    id = e.target.id;
+    tasks.markFinished(id);
+  } else if (e.target.className === "fa fa-check-circle" ){ 
+    id = e.target.parentElement.id;
+    tasks.markFinished(id);
+  } else if (e.target.className === "fa fa-trash-alt") {
+    id = e.target.parentElement.id;
+    tasks.delete(id);
+  }
+});
